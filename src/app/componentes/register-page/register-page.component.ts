51eee87c63d609ddc005295115033951f6ce4e86
Jose Banegas <josbanze@gmail.com>
2019-03-29 12:58:17 -0600

    Creado servicio AUTH,el registro de usuarios listo

--- src/app/componentes/register-page/register-page.component.ts
+++ src/app/componentes/register-page/register-page.component.ts
@@ -1,15 +1,30 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { AuthService } from "../../servicios/auth.service";
+import { Router } from "@angular/router";
 
 @Component({
-  selector: 'app-register-page',
-  templateUrl: './register-page.component.html',
-  styleUrls: ['./register-page.component.scss']
+  selector: "app-register-page",
+  templateUrl: "./register-page.component.html",
+  styleUrls: ["./register-page.component.scss"]
 })
 export class RegisterPageComponent implements OnInit {
+  public email: string;
+  public password: string;
+  constructor(private autService: AuthService, private router: Router) {}
 
-  constructor() { }
+  ngOnInit() {}
 
-  ngOnInit() {
+  onSubmitAddUser() {
+    this.autService
+      .registerUser(this.email, this.password)
+      .then(res => {
+        console.log("Usuario Creado correctamente");
+        alert("Usuario Creado correctamente");
+        this.router.navigate(["/privado"]);
+      })
+      .catch(err => {
+        alert("No registro el usuario correctamente");
+        console.log(err);
+      });
   }
-
 }
