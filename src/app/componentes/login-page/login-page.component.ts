e3d561899cbdd77b0ceddc848f6e42c0e9b99122
Jose Banegas <josbanze@gmail.com>
2019-03-29 13:57:55 -0600

    Añadimos login y logout

--- src/app/componentes/login-page/login-page.component.ts
+++ src/app/componentes/login-page/login-page.component.ts
@@ -1,15 +1,31 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { AuthService } from "../../servicios/auth.service";
+import { Router } from "@angular/router";
 
 @Component({
-  selector: 'app-login-page',
-  templateUrl: './login-page.component.html',
-  styleUrls: ['./login-page.component.scss']
+  selector: "app-login-page",
+  templateUrl: "./login-page.component.html",
+  styleUrls: ["./login-page.component.scss"]
 })
 export class LoginPageComponent implements OnInit {
+  public email: string;
+  public password: string;
 
-  constructor() { }
+  constructor(private autServices: AuthService, private router: Router) {}
 
-  ngOnInit() {
+  ngOnInit() {}
+
+  onSubmitLogin() {
+    this.autServices
+      .loginEmail(this.email, this.password)
+      .then(res => {
+        alert("Logueado correctamente");
+        this.router.navigate(["/privado"]);
+      })
+      .catch(err => {
+        alert("Credenciales incorrectas, favor veifique");
+        //console.log(err);
+        this.router.navigate(["/login"]);
+      });
   }
-
 }
--- src/app/componentes/navbar/navbar.component.ts
+++ src/app/componentes/navbar/navbar.component.ts
@@ -1,15 +1,31 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { AuthService } from "../../servicios/auth.service";
 
 @Component({
-  selector: 'app-navbar',
-  templateUrl: './navbar.component.html',
-  styleUrls: ['./navbar.component.scss']
+  selector: "app-navbar",
+  templateUrl: "./navbar.component.html",
+  styleUrls: ["./navbar.component.scss"]
 })
 export class NavbarComponent implements OnInit {
+  public isLogin: boolean;
+  public nombreUsuario: string;
+  public emailUsuario: string;
 
-  constructor() { }
+  constructor(private AuthService: AuthService) {}
 
   ngOnInit() {
+    this.AuthService.getAuth().subscribe(auth => {
+      if (auth) {
+        this.isLogin = true;
+        this.nombreUsuario = auth.displayName;
+        this.emailUsuario = auth.email;
+      } else {
+        this.isLogin = false;
+      }
+    });
   }
 
+  onClickLogout() {
+    this.AuthService.logout();
+  }
 }
